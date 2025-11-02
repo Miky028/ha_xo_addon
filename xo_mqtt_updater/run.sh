e5beb9f3f9184b7ad1dc6ebf835c306356d42dff
Miky028 <40164412+Miky028@users.noreply.github.com>
2025-11-02 18:10:56 +0100

    Update run.sh

--- xo_mqtt_updater/run.sh
+++ xo_mqtt_updater/run.sh
@@ -4,9 +4,9 @@ set -e
 CONFIG_FILE="/data/options.json"
 
 XO_URL=$(jq -r '.xo_url' $CONFIG_FILE)
-HOST_UUID=$(jq -r '.host_uuid' $CONFIG_FILE)
-USERNAME=$(jq -r '.username' $CONFIG_FILE)
-PASSWORD=$(jq -r '.password' $CONFIG_FILE)
+XO_TOKEN=$(jq -r '.xo_token' $CONFIG_FILE)
+VERIFY_SSL=$(jq -r '.verify_ssl' $CONFIG_FILE)
+OBJECTS=$(jq -c '.objects' $CONFIG_FILE)
 MQTT_HOST=$(jq -r '.mqtt_host' $CONFIG_FILE)
 MQTT_PORT=$(jq -r '.mqtt_port' $CONFIG_FILE)
 MQTT_USER=$(jq -r '.mqtt_user' $CONFIG_FILE)
@@ -17,11 +17,11 @@ echo "[RUN.SH] Spouštím XO MQTT updater..."
 
 exec python /xo_updater.py \
     --xo_url "$XO_URL" \
-    --host_uuid "$HOST_UUID" \
-    --username "$USERNAME" \
-    --password "$PASSWORD" \
+    --xo_token "$XO_TOKEN" \
+    --verify_ssl "$VERIFY_SSL" \
     --mqtt_host "$MQTT_HOST" \
     --mqtt_port "$MQTT_PORT" \
     --mqtt_user "$MQTT_USER" \
     --mqtt_password "$MQTT_PASSWORD" \
-    --interval "$UPDATE_INTERVAL"
+    --update_interval "$UPDATE_INTERVAL" \
+    --objects "$OBJECTS"
