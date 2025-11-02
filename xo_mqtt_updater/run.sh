52ea29227abfa5c1a9ed2e041517a80a9fadfab3
Miky028 <40164412+Miky028@users.noreply.github.com>
2025-11-02 16:15:46 +0100

    Update run.sh

--- xo_mqtt_updater/run.sh
+++ xo_mqtt_updater/run.sh
@@ -4,7 +4,8 @@ XO_URL=${XO_URL:-"http://xo.local"}
 HOST_UUID=${HOST_UUID:-""}
 USERNAME=${USERNAME:-"admin"}
 PASSWORD=${PASSWORD:-"password"}
-MQTT_SERVER=${MQTT_SERVER:-"mqtt://core-mosquitto"}
+MQTT_HOST=${MQTT_HOST:-"core-mosquitto"}
+MQTT_PORT=${MQTT_PORT:-1883}
 MQTT_USER=${MQTT_USER:-""}
 MQTT_PASSWORD=${MQTT_PASSWORD:-""}
 UPDATE_INTERVAL=${UPDATE_INTERVAL:-30}
@@ -15,7 +16,8 @@ exec python /xo_updater.py \
     --host_uuid "$HOST_UUID" \
     --username "$USERNAME" \
     --password "$PASSWORD" \
-    --mqtt_server "$MQTT_SERVER" \
+    --mqtt_host "$MQTT_HOST" \
+    --mqtt_port "$MQTT_PORT" \
     --mqtt_user "$MQTT_USER" \
     --mqtt_password "$MQTT_PASSWORD" \
     --interval "$UPDATE_INTERVAL"
