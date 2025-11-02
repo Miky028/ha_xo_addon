5ad9551efdfb99d62affe57344ac071860ee0788
Miky028 <40164412+Miky028@users.noreply.github.com>
2025-11-02 16:07:38 +0100

    Update run.sh

--- xo_mqtt_updater/run.sh
+++ xo_mqtt_updater/run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-# Home Assistant add-on start script
 
 XO_URL=${XO_URL:-"http://xo.local"}
 HOST_UUID=${HOST_UUID:-""}
 USERNAME=${USERNAME:-"admin"}
 PASSWORD=${PASSWORD:-"password"}
 MQTT_SERVER=${MQTT_SERVER:-"mqtt://core-mosquitto"}
+MQTT_USER=${MQTT_USER:-""}
+MQTT_PASSWORD=${MQTT_PASSWORD:-""}
 UPDATE_INTERVAL=${UPDATE_INTERVAL:-30}
 
 echo "Spouštím XO MQTT updater..."
@@ -15,4 +16,6 @@ exec python /xo_updater.py \
     --username "$USERNAME" \
     --password "$PASSWORD" \
     --mqtt_server "$MQTT_SERVER" \
+    --mqtt_user "$MQTT_USER" \
+    --mqtt_password "$MQTT_PASSWORD" \
     --interval "$UPDATE_INTERVAL"
